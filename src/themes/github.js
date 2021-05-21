0040cd472d20cb8863a1740ee88fd276e8497601
Phil Pluckthun <phil@kitten.sh>
2021-05-21 20:10:39 +0100

    Revert "Update github.js (#100)"
    This reverts commit 1b77bf40a16e9b74302217a8b040af6fd0be6e51.

--- src/themes/github.js
+++ src/themes/github.js
@@ -1,168 +1,78 @@
 // @flow
+// Original: https://raw.githubusercontent.com/PrismJS/prism-themes/master/themes/prism-ghcolors.css
 
 import type { PrismTheme } from "../types";
 
 var theme: PrismTheme = {
   plain: {
-    color: "rgb(36, 41, 46)",
+    color: "#393A34",
     backgroundColor: "#f6f8fa",
   },
   styles: [
     {
-      types: ["prolog"],
+      types: ["comment", "prolog", "doctype", "cdata"],
       style: {
-        color: "rgb(0, 0, 128)",
+        color: "#999988",
+        fontStyle: "italic",
       },
     },
     {
-      types: ["comment"],
+      types: ["namespace"],
       style: {
-        color: "rgb(106, 153, 85)",
+        opacity: 0.7,
       },
     },
     {
-      types: ["builtin", "changed", "keyword"],
+      types: ["string", "attr-value"],
       style: {
-        color: "rgb(215, 58, 73)",
         color: "#e3116c",
-      },
-    },
-    {
-      types: ["inserted-sign", "inserted"],
-      style: {
-        backgroundColor: "rgb(240, 255, 244)",
-        color: "rgb(34, 134, 58)",
-      },
-    },
-    {
-      types: ["constant"],
-      style: {
-        color: "rgb(100, 102, 149)",
-      },
-    },
-    {
-      types: ["attr-name", "variable"],
-      style: {
-        color: "rgb(156, 220, 254)",
-      },
-    },
-    {
-      types: ["deleted-sign", "deleted"],
-      style: {
-        backgroundColor: "rgb(255, 238, 240)",
-        color: "rgb(179, 29, 40)",
-      },
-    },
-    {
-      types: ["selector"],
-      style: {
-        color: "rgb(215, 186, 125)",
-      },
-    },
-    {
-      // Fix tag color
-      types: ["tag"],
-      style: {
-        color: "rgb(78, 201, 176)",
-      },
-    },
-    {
-      // Fix tag color for HTML
-      types: ["tag"],
-      languages: ["markup"],
-      style: {
-        color: "rgb(86, 156, 214)",
       },
     },
     {
       types: ["punctuation", "operator"],
       style: {
-        color: "rgb(36, 41, 46)",
+        color: "#393A34",
       },
     },
     {
-      types: ["operator"],
+      types: [
+        "entity",
+        "url",
+        "symbol",
+        "number",
+        "boolean",
+        "variable",
+        "constant",
+        "property",
+        "regex",
+        "inserted",
+      ],
       style: {
-        color: "rgb(0, 92, 197)",
+        color: "#36acaa",
       },
     },
     {
-      types: ["boolean"],
+      types: ["atrule", "keyword", "attr-name", "selector"],
       style: {
-        color: "rgb(0, 92, 197)",
+        color: "#00a4db",
       },
     },
     {
-      // Fix punctuation color for HTML
-      types: ["punctuation"],
-      languages: ["markup"],
+      types: ["function", "deleted", "tag"],
       style: {
-        color: "#808080",
+        color: "#d73a49",
       },
     },
     {
-      types: ["function"],
+      types: ["function-variable"],
       style: {
-        color: "rgb(111, 66, 193)",
+        color: "#6f42c1",
       },
     },
     {
-      types: ["class-name"],
+      types: ["tag", "selector", "keyword"],
       style: {
-        color: "rgb(78, 201, 176)",
-      },
-    },
-    {
-      types: ["known-class-name", "class-name"],
-      style: {
-        color: "rgb(227, 98, 9)",
-      },
-    },
-    {
-      types: ["char"],
-      style: {
-        color: "rgb(209, 105, 105)",
-      },
-    },
-    {
-      types: ["property-access"],
-      style: {
-        color: "rgb(0, 92, 197)",
-      },
-    },
-    {
-      types: ["method", "function", "property-access"],
-      style: {
-        color: "rgb(111, 66, 193)",
-      },
-    },
-    {
-      languages: ["json"],
-      types: ["property"],
-      style: {
-        color: "rgb(3, 47, 98)",
-      },
-    },
-    {
-      languages: ["json"],
-      types: ["string"],
-      style: {
-        color: "rgb(3, 47, 98)",
-      },
-    },
-    {
-      languages: ["json"],
-      types: ["number"],
-      style: {
-        color: "rgb(0, 92, 197)",
-      },
-    },
-    {
-      languages: ["json"],
-      types: ["comment"],
-      style: {
-        backgroundColor: "rgb(179, 29, 40)",
-        color: "rgb(250, 251, 252)",
+        color: "#00009f",
       },
     },
   ],
