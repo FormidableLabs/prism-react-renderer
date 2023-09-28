1af270742bcb4ddd27bda2c8f1095428fa8d4258
Carlos Kelly <me@carlos.dev>
2023-09-28 11:41:30 -0500

    Add Python support.

--- packages/demo/src/sample-code.ts
+++ packages/demo/src/sample-code.ts
@@ -108,6 +108,33 @@ end if
     `,
   },
 
+  ["Python"]: {
+    language: "python",
+    code: `
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, classification_report
+
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+clf = DecisionTreeClassifier(random_state=42)
+clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+report = classification_report(y_test, y_pred)
+print(f"Accuracy: {accuracy}")
+print("Classification Report:\\n", report)
+print("Feature Importances:", clf.feature_importances_)
+    `,
+  },
+
   ["Rust"]: {
     language: "rust",
     code: `
